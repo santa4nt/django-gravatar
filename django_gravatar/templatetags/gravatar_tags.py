66709786eb731a02618f73a509a317ab85e86ceb
Santoso Wijaya <santa.ant@me.com>
2010-04-21 12:55:03 -0700

    Refactor the building of Gravatar URL to a function.

--- django_gravatar/templatetags/gravatar_tags.py
+++ django_gravatar/templatetags/gravatar_tags.py
@@ -22,6 +22,60 @@ DEFAULT_PARAMS = \
 register = template.Library()
 
 
+def _build_gravatar_url(email, params):
+    """Generate a Gravatar URL."""
+    # step 1: get a hex hash of the email address
+    email = email.strip().lower().encode('utf-8')
+    email_hash = hashlib.md5(email).hexdigest()
+
+    # step 2a: build a canonized parameters dictionary
+    if not type(params).__name__ == 'dict':
+        params = params.__dict__
+
+    actual_params = {}
+    default_keys = DEFAULT_PARAMS.keys()
+    for key, value in params.items():
+        if key in default_keys:
+            k, default_value = DEFAULT_PARAMS[key]
+            # skip parameters whose values are defaults,
+            # assume these values are mirroring Gravatar's defaults
+            if value != default_value:
+                actual_params[k] = value
+
+    # step 2b: validate the canonized parameters dictionary
+    # silently drop parameter when the value is not valid
+    for key, value in actual_params.items():
+        if key == 's':
+            if value < 1 or value > 512:
+                del actual_params[key]
+        elif key == 'r':
+            if value.lower() not in ('g', 'pg', 'r', 'x'):
+                del actual_params[key]
+        # except when the parameter key is 'd': replace with 'identicon'
+        elif key == 'd':
+            if value.lower() not in ('identicon', 'monsterid', 'wavatar', '404'):
+                if not URL_RE.match(value): # if not a valid URI
+                    del actual_params[key]
+                else:                       # valid URI, encode it
+                    actual_params[key] = value  # urlencode will encode it later
+    
+    # set parameter d=identicon if missing
+    try:
+        actual_params['d']
+    except KeyError:
+        actual_params['d'] = 'identicon'
+
+    # step 3: encode params
+    params_encode = urllib.urlencode(actual_params)
+
+    # step 4: form the gravatar url
+    gravatar_url = GRAVATAR_URL_PREFIX + email_hash
+    if params_encode:
+        gravatar_url += '?' + params_encode
+
+    return gravatar_url
+
+
 class GravatarURLNode(template.Node):
 
     def __init__(self, email, params):
@@ -36,12 +90,13 @@ class GravatarURLNode(template.Node):
                 params = {}
 
             # try matching an address string literal
-            if EMAIL_RE.match(self.email.strip()):
-                email = self.email.strip().lower().encode('utf-8')
+            email_literal = self.email.strip().lower()
+            if EMAIL_RE.match(email_literal):
+                email = email_literal
             # treat as a variable
             else:
                 email = template.Variable(self.email).resolve(context)
-                email = email.strip().lower().encode('utf-8')
+                email = email.strip().lower()
                 if not EMAIL_RE.match(email):
                     raise template.TemplateSyntaxError('Not a valid email address.')
         except template.VariableDoesNotExist:
@@ -53,55 +108,7 @@ class GravatarURLNode(template.Node):
                 return ''
 
         # now, we generate the gravatar url
-        # step 1: get a hex hash of the email address
-        email_hash = hashlib.md5(email).hexdigest()
-
-        # step 2a: build a canonized parameters dictionary
-        if not type(params).__name__ == 'dict':
-            params = params.__dict__
-
-        actual_params = {}
-        default_keys = DEFAULT_PARAMS.keys()
-        for key, value in params.items():
-            if key in default_keys:
-                k, default_value = DEFAULT_PARAMS[key]
-                # skip parameters whose values are defaults,
-                # assume these values are mirroring Gravatar's defaults
-                if value != default_value:
-                    actual_params[k] = value
-
-        # step 2b: validate the canonized parameters dictionary
-        # silently drop parameter when the value is not valid
-        for key, value in actual_params.items():
-            if key == 's':
-                if value < 1 or value > 512:
-                    del actual_params[key]
-            elif key == 'r':
-                if value.lower() not in ('g', 'pg', 'r', 'x'):
-                    del actual_params[key]
-            # except when the parameter key is 'd': replace with 'identicon'
-            elif key == 'd':
-                if value.lower() not in ('identicon', 'monsterid', 'wavatar', '404'):
-                    if not URL_RE.match(value): # if not a valid URI
-                        del actual_params[key]
-                    else:                       # valid URI, encode it
-                        actual_params[key] = value  # urlencode will encode it later
-        
-        # set parameter d=identicon if missing
-        try:
-            actual_params['d']
-        except KeyError:
-            actual_params['d'] = 'identicon'
-
-        # step 3: encode params
-        params_encode = urllib.urlencode(actual_params)
-
-        # step 4: form the gravatar url
-        gravatar_url = GRAVATAR_URL_PREFIX + email_hash
-        if params_encode:
-            gravatar_url += '?' + params_encode
-
-        return gravatar_url
+        return _build_gravatar_url(email, params)
 
 
 @register.tag(name="gravatar_url")
